76688736cee1f5e6be9c3260723fc33e4c5e8759
Shuo Tian <tianshuo1996@outlook.com>
2022-02-27 22:23:20 -0600

    change table lib

--- component/OverviewTable.tsx
+++ component/OverviewTable.tsx
@@ -1,67 +1,26 @@
-/* eslint-disable react/jsx-key */
-// @ts-nocheck
 import React, { useMemo } from "react";
+import DataTable from "react-data-table-component";
 import { Overview } from "../types";
-import { Table, Thead, Tbody, Tr, Th, Td, chakra } from "@chakra-ui/react";
-import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
-import { useTable, useSortBy } from "react-table";
 
 interface TableProps {
   overviews: Overview[];
 }
 
 const OverviewTable: React.FC<TableProps> = ({ overviews }) => {
+  const sortableCols = useMemo(
+    () => ["date_added", "category.name", "user.username", "project_title"],
+    []
+  );
   const columns = useMemo(
     () =>
-      Object.keys(overviews[0]).map((key) => ({
-        Header: key,
-        accessor: key,
+      Object.keys(overviews[0]).map((colName) => ({
+        name: colName,
+        selector: (row: any) => row[colName],
+        sortable: sortableCols.includes(colName),
       })),
-    [overviews]
+    [overviews, sortableCols]
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ data: overviews, columns }, useSortBy);
-  return (
-    <Table {...getTableProps()}>
-      <Thead>
-        {headerGroups.map((headerGroup) => (
-          <Tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <Th
-                {...column.getHeaderProps(column.getSortByToggleProps())}
-                isNumeric={column.isNumeric}
-              >
-                {column.render("Header")}
-                <chakra.span pl="4">
-                  {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      <TriangleDownIcon aria-label="sorted descending" />
-                    ) : (
-                      <TriangleUpIcon aria-label="sorted ascending" />
-                    )
-                  ) : null}
-                </chakra.span>
-              </Th>
-            ))}
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <Tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <Td {...cell.getCellProps()} isNumeric={cell.column.isNumeric}>
-                  {cell.render("Cell")}
-                </Td>
-              ))}
-            </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
-  );
+  return <DataTable columns={columns} data={overviews} />;
 };
 export default OverviewTable;
